77395462120d41f40c3c0595772f0d71bc81e3ed
cyber6 <nakisa.donnelly@gmail.com>
2023-01-30 13:56:57 -0800

    player crouch ability

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [Header("Crouch Settings")]
+    public InputActionReference crouchButton = null;
+    public float crouchHeight = 0.7f;
+    public float crouchDuration = 0.75f;
+    public Transform cameraOffsetTransform;
+
+    private bool crouch = false;
+    private Vector3 crouchPosition;
     public static Player Instance
     {
         get;
@@ -26,6 +36,42 @@ public class Player : MonoBehaviour
         }
 
         Instance = this;
+
+        crouchButton.action.performed += CrouchUncrouch;
+    }
+
+    private void OnDestroy()
+    {
+        crouchButton.action.performed -= CrouchUncrouch;
+    }
+
+    private void CrouchUncrouch(InputAction.CallbackContext obj)
+    {
+        crouch = !crouch;
+        
+        if (crouch)
+        {
+            this.crouchPosition = cameraOffsetTransform.position;
+            Vector3 crouchPosition = new Vector3(cameraOffsetTransform.position.x, cameraOffsetTransform.position.y - crouchHeight, cameraOffsetTransform.position.z);
+            StartCoroutine(CrouchUnCrouch(cameraOffsetTransform.position, crouchPosition));
+        }
+        else
+        {
+            Vector3 uncrouch = this.crouchPosition;
+            StartCoroutine(CrouchUnCrouch(cameraOffsetTransform.position, uncrouch));
+        }
+    }
+
+    private IEnumerator CrouchUnCrouch(Vector3 startPosition, Vector3 endPosition)
+    {
+        float elapsedTime = 0.0f;
+        while (elapsedTime <= crouchDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            Vector3 lerpPosition = Vector3.Lerp(startPosition, endPosition, elapsedTime / crouchDuration);
+            cameraOffsetTransform.position = lerpPosition;
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     public Vector3 PlayerPosition()
